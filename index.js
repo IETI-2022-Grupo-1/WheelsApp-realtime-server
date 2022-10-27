823e5072e00ec821dc65d8da78dc053becccd18c
Juan Murillo <jdmurillo@google.com>
2022-10-27 14:49:35 -0500

    PORT env added

--- index.js
+++ index.js
@@ -31,4 +31,5 @@ io.on('connection', socket => {
     })
 });
 
-server.listen(process.env.SERVER_PORT, () => console.log(`Server running on port: ${process.env.SERVER_PORT}`))
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Server running on port: ${port}`))
